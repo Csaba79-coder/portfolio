144156dbbc04f715862224c4d092b9c38bf1f3bc
Csaba Vadász <csabavadasz79@gmail.com>
2024-12-25 18:54:09 +0100

    update portfolio page adding resumé in 4 languages

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className='footer'>
-            Copyright © Csaba Vadasz - Csaba79-coder 2023
+            Copyright © Csaba Vadasz - Csaba79-coder 2025
         </footer>
     );
 }
--- src/components/resume.js
+++ src/components/resume.js
@@ -8,10 +8,42 @@ class Resume extends Component {
   render() {
     return(
         <div className="landing-container">
-          <div className="background-image"></div>
+            <div className="background-image"></div>
+            <div className="resume-downloads">
+                {/* Avatar and Introduction */}
+                <div className="intro">
+                    <img src={avatarImg} alt="Avatar" className="avatar"/>
+                    <h1>Csaba Vadasz</h1>
+                    <h2>Full Stack Developer</h2>
+                    <h4>
+                        I am an enthusiastic Java-Spring software engineer, primarily a backend software developer,
+                        interested in the development (technology) of Artificial Intelligence (AI).
+                    </h4>
+                </div>
 
+                {/* Resume Downloads Section */}
+                <Grid className="resume-grid">
+                    <Cell col={12}>
+                        <h3>Download my resumé:</h3>
+                        <div className="resume-buttons">
+                            <a href="/assets/csaba_vadasz_resume_eng.pdf" download>
+                                <button className="resume-button">Resume (English)</button>
+                            </a>
+                            <a href="/assets/csaba_vadasz_resume_hun.pdf" download>
+                                <button className="resume-button">Önéletrajz (magyar)</button>
+                            </a>
+                            <a href="/assets/csaba_vadasz_resume_deu.pdf" download>
+                                <button className="resume-button">Lebenslauf (Deutsch)</button>
+                            </a>
+                            <a href="/assets/csaba_vadasz_resume_ita.pdf" download>
+                                <button className="resume-button">Curriculum Vitae (Italiano)</button>
+                            </a>
+                        </div>
+                    </Cell>
+                </Grid>
 
-          <Footer />
+                <Footer/>
+            </div>
         </div>
     )
   }
