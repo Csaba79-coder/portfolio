04d33df6bf08765476a04644c134644c1f8ee72c
Csaba Vadász <csabavadasz79@gmail.com>
2026-01-09 13:06:30 +0100

    update footer to year 2026

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className='footer'>
-            Copyright © Csaba Vadasz - Csaba79-coder 2025
+            Copyright © Csaba Vadasz - Csaba79-coder 2026
         </footer>
     );
 }
